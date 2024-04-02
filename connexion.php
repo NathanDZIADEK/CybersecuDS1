6fcb345875f687a339b6758cf914d300074459ad
Nathan Dziadek <nathandziadek2003@gmail.com>
2024-04-02 17:44:12 +0200

    CybersecuDS

--- connexion.php
+++ connexion.php
@@ -3,19 +3,24 @@
 <form method="POST" action="recupConnexion.php">
 <p>
 <label>Nom: </label>
-<input type="text" name="nom">
+<input type="text" name="nom" maxlength="30" required>
 </p>
 <p>
 <label>Email: </label>
-<input type="text" name="email">
+<input type="text" name="email" maxlength="50" required>
 </p>
 <p>
 <label>Age: </label>
-<input type="text" name="age"/>
+<input type="text" name="age" maxlength="30" required>
 </p>
 <p>
 <label>Commentaire: </label>
-<Textarea type="commentaire" name="commentaire">
+<Textarea type="commentaire" name="commentaire" rows="10" cols="60" null="no" placeholder="Description..."></textarea>
 </p>
+<p>
+<div class="bouton">
+<input type="submit" value="Ajouter" name="ajout" null="no"/>
+</div>
+
 </form>
 </fieldset>
--- recupConnexion.php
+++ recupConnexion.php
@@ -14,21 +14,29 @@ $commentaire=filter_input(INPUT_POST,'commentaire',FILTER_SANITIZE_STRING);
     echo "Age : $age <br>";
     echo "Commentaire : $commentaire <br>";
     if (ctype_alpha($nom)) {
-        echo "le nom n'est pas vide et ne contient que des lettres.", '<br>';
+        echo "le nom '$nom' est en cours de validation.", '<br>';
     } else {
-        echo "le nom est vide et ne contient pas que des lettres.", '<br>';
+        echo "le nom '$nom' est vide ou contient des chiffres.", '<br>';
     }
     if (empty($nom)) {
-        echo "veuillez écrire le nom attendu.", '<br>';
-    } 
+        echo "le nom '$nom' ne peut pas être vide.", '<br>';
+    } else {
+        echo "le nom '$nom' est entièrement vérifié.", '<br>';
+    }
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "L'adresse mail '$email' est valide.", '<br>';
+    } else {
+        echo "L'adresse mail '$email' n'est pas valide", '<br>';
     }
-    if (is_numeric($age)) {
-        echo "l'age '$age' est considéré comme un entier.", '<br>';
+    if (is_numeric($age) && $age > 18) {
+        echo "l'age '$age' est considéré comme un entier et est supérieur à 18.", '<br>';
+    } else {
+        echo "l'age '$age' n'est pas considéré comme un entier ou n'est pas supérieur à 18.", '<br>';
     }
-    if (strlen($age)<18) {
-        echo "l'age '$age' est supérieur ou égal à 18 ans.", '<br>';
+    if (empty($commentaire)) {
+        echo "le commentaire '$commentaire' n'est pas vide.", '<br>'; 
+    } else {
+        echo "le commentaire '$commentaire' est vide.", '<br>';
     }
         
     
